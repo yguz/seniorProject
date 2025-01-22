c570807ec8493239812392333bfb437cf84439fb
yguz <yorfig@gmail.com>
2025-01-21 23:43:35 -0500

    fixed dropdown and added css styles and search bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
 import './Navbar.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
   return (
-
-<nav className="navbar">
-  <div className="navbar-left">
-    <a href="/" className="logo">
-      On-a-Budget Meals
-    </a>
-  </div>
-  <div className="navbar-center">
-      <ul className='nav-links'>
-          <li><a href="#Home">Home</a></li>
-          {/* <li class="dropdown">
-            <a href="#Meals" class="dropbtn">Meals</a>
-            <div class="dropdown-content">
-              <a href="#breakfast">Breakfast</a>
-              <a href="#lunch">Lunch</a>
-              <a href="#dinner">Dinner</a>
-            </div>
-          </li> */}
-          <li><a href="#About Us">About Us</a></li>
-          <li><a href="#Contact Us">Contact Us</a></li>
-      </ul>
-  </div>
-</nav>
-);
+    <nav className="navbar navbar-expand-lg navbar-light bg-beige">
+      <div className="container-fluid">
+        <a href="/" className="navbar-brand">
+          <img src="src/assets/logo.png" alt="Logo" className="navbar-logo" />
+        </a>
+        
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#Home">Home</a>
+            </li>
+            <li className="nav-item">
+              <DropdownButton id="dropdown" title="Meals" className="nav-link">
+                <Dropdown.Item href="#breakfast">Breakfast</Dropdown.Item>
+                <Dropdown.Item href="#lunch">Lunch</Dropdown.Item>
+                <Dropdown.Item href="#dinner">Dinner</Dropdown.Item>
+              </DropdownButton>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#About Us">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#Contact Us">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-// import './Search.css';
+import './Search.css';
 
 const Search = () => {
   return (
-      <div>
-            <h1>This is the search.</h1>
-      </div>
-);
+    <div className="search-container">
+      <div className="search-input-container">
+          <input type="text" placeholder="Type to search" className="search-input" />
+          <button className="btn btn-dark">Search</button>
+        </div>
+    </div>
+  );
 };
 
 export default Search;
